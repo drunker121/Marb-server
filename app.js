cd2b79c6b4930c4becd013769db275af87dd217f
drunker121 <87312371+drunker121@users.noreply.github.com>
2023-03-06 16:08:41 +0530

    keys4

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(
     cors({
         origin: ['https://architectinsider.in', 'http://localhost:3000'],
-        // credentials: true,
+        credentials: true,
         withCredentials: true
     })
 );
